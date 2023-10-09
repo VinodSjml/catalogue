2a80ef56799b71aea174ba12ad252c01f48a32c2
CodingManoj <mvp2612@hotmail.com>
2023-10-08 23:32:01 -0400

    Git Auto Push

--- mutable-infra/main.tf
+++ mutable-infra/main.tf
@@ -8,5 +8,6 @@ module "catalogue" {
   OD_INSTANCE_TYPE     = var.OD_INSTANCE_TYPE
   OD_INSTANCE_COUNT    = var.OD_INSTANCE_COUNT
   APP_PORT             = var.APP_PORT
+  APP_VERSION          = var.APP_VERSION
   INTERNAL             = var.INTERNAL 
 }
